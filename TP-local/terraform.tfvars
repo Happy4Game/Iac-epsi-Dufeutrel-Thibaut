42f775890e50bcdae104f86930d94a74ac6d3277
Happy4Game <dufeutrel.thibaut@gmail.com>
2026-03-06 16:17:34 +0100

    feat: exo 7 finished

---
+++ TP-local/terraform.tfvars
@@ -0,0 +1,25 @@
+container_name  = "nginx-terraform"
+image_name      = "nginx:latest"
+internal_port   = 80
+external_port   = 8080
+
+machines = [
+  {
+    name      = "web-server-1"
+    vcpu      = 2
+    disk_size = 20
+    region    = "eu-west-1"
+  },
+  {
+    name      = "web-server-2"
+    vcpu      = 4
+    disk_size = 50
+    region    = "us-east-1"
+  },
+  {
+    name      = "database-server"
+    vcpu      = 8
+    disk_size = 100
+    region    = "ap-southeast-1"
+  }
+]
--- TP-local/variables.tf
+++ TP-local/variables.tf
@@ -22,3 +22,34 @@ variable external_port {
   description = "External port"
 }
 
+variable machines {
+  type = list(object({
+    name      = string
+    vcpu      = number
+    disk_size = number
+    region    = string
+  }))
+  description = "List of virtual machines to deploy"
+  
+  validation {
+    condition = alltrue([
+      for machine in var.machines : machine.vcpu >= 2 && machine.vcpu <= 64
+    ])
+    error_message = "vCPU count must be between 2 and 64."
+  }
+  
+  validation {
+    condition = alltrue([
+      for machine in var.machines : machine.disk_size >= 20
+    ])
+    error_message = "Disk size must be at least 20 GB."
+  }
+  
+  validation {
+    condition = alltrue([
+      for machine in var.machines : contains(["eu-west-1", "us-east-1", "ap-southeast-1"], machine.region)
+    ])
+    error_message = "Region must be one of: eu-west-1, us-east-1, ap-southeast-1."
+  }
+}
+
